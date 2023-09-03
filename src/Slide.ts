3d134c790fe0fa0bfd078ce14075c450d1a8f0c9
Jean Tsushima <imjeantsushima@gmail.com>
2023-09-03 00:55:02 -0300

    chore: create slide class

---
+++ src/Slide.ts
@@ -0,0 +1,29 @@
+export default class Slide {
+  container: Element;
+  slides: Element[];
+  controls: Element;
+  timer: number;
+  index: number;
+  slide: Element;
+
+  constructor(container: Element, slides: Element[], controls: Element, timer: number = 5000) {
+    this.container = container;
+    this.slides = slides;
+    this.controls = controls;
+    this.timer = timer;
+
+    this.index = 0;
+    this.slide = this.slides[this.index]
+
+    this.show(this.index)
+  }
+  hide(el: Element) {
+    el.classList.remove('active')
+  };
+  show(index: number) {
+    this.index = index;;
+    this.slide = this.slides[this.index];
+    this.slides.forEach(el => this.hide(el));
+    this.slide.classList.add('active');
+  };
+}
