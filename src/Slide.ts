1c4078d1fd440724bd3bb6d8e9b686351920e514
Jean Tsushima <imjeantsushima@gmail.com>
2023-09-03 23:07:50 -0300

    chore: handle video in stories

--- src/Slide.ts
+++ src/Slide.ts
@@ -19,22 +19,41 @@ export default class Slide {
 
     this.timeout = null;
     this.pausedTimeout = null;
-    this.index = 0;
+    this.index = localStorage.getItem('activeSlide') ? Number(localStorage.getItem('activeSlide')) : 0;
     this.slide = this.slides[this.index]
 
     this.paused = false;
     this.init();
   }
   hide(el: Element) {
-    el.classList.remove('active')
+    el.classList.remove('active');
+    if (el instanceof HTMLVideoElement) {
+      el.currentTime = 0;
+      el.pause();
+    }
   };
   show(index: number) {
     this.index = index;
     this.slide = this.slides[this.index];
+    localStorage.setItem("activeSlide", String(this.index));
+
     this.slides.forEach(el => this.hide(el));
     this.slide.classList.add('active');
-    this.auto(this.timer)
+    if (this.slide instanceof HTMLVideoElement) {
+      this.autovideo(this.slide)
+    } else {
+      this.auto(this.timer)
+    }
   };
+  autovideo(video: HTMLVideoElement) {
+    video.muted = true;
+    video.play();
+    let firstPlay = true;
+    video.addEventListener('playing', () => {
+      if (firstPlay) this.auto(video.duration * 1000);
+      firstPlay = false;
+    })
+  }
   auto(timer: number) {
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), timer);
@@ -50,10 +69,10 @@ export default class Slide {
     this.show(next)
   }
   pause() {
-    console.log('paused');
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause();
       this.paused = true;
+      if (this.slide instanceof HTMLVideoElement) this.slide.pause();
     }, 300)
   }
   continue() {
@@ -61,6 +80,7 @@ export default class Slide {
     this.pausedTimeout?.clear();
     this.paused = false;
     this.timeout?.continue()
+    if (this.slide instanceof HTMLVideoElement) this.slide.play();
   }
   private addControls() {
     const prevButton = document.createElement('button');
