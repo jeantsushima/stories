fee81b7115ceea387c47633a7417f4513ec60d24
Jean Tsushima <imjeantsushima@gmail.com>
2023-09-03 22:56:07 -0300

    chore: automatic timeout with pause and continue

--- src/Slide.ts
+++ src/Slide.ts
@@ -8,6 +8,8 @@ export default class Slide {
   index: number;
   slide: Element;
   timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
 
   constructor(container: Element, slides: Element[], controls: Element, timer: number = 5000) {
     this.container = container;
@@ -16,9 +18,11 @@ export default class Slide {
     this.timer = timer;
 
     this.timeout = null;
+    this.pausedTimeout = null;
     this.index = 0;
     this.slide = this.slides[this.index]
 
+    this.paused = false;
     this.init();
   }
   hide(el: Element) {
@@ -36,14 +40,28 @@ export default class Slide {
     this.timeout = new Timeout(() => this.next(), timer);
   }
   prev() {
+    if (this.paused) return;
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
     this.show(prev);
   }
   next() {
+    if (this.paused) return;
     const next = (this.index + 1) < this.slides.length ? this.index + 1 : 0;
     this.show(next)
   }
-
+  pause() {
+    console.log('paused');
+    this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
+      this.paused = true;
+    }, 300)
+  }
+  continue() {
+    console.log('continue');
+    this.pausedTimeout?.clear();
+    this.paused = false;
+    this.timeout?.continue()
+  }
   private addControls() {
     const prevButton = document.createElement('button');
     const nextButton = document.createElement('button');
@@ -51,6 +69,9 @@ export default class Slide {
     nextButton.innerText = 'Próximo';
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener('pointerdown', () => this.pause())
+    this.controls.addEventListener('pointerup', () => this.continue())
 
     nextButton.addEventListener('pointerup', () => this.next())
     prevButton.addEventListener('pointerup', () => this.prev())
--- src/Timeout.ts
+++ src/Timeout.ts
@@ -1,11 +1,25 @@
 export default class Timeout {
   id;
   handler;
+  start;
+  timeLeft;
   constructor(handler: TimerHandler, timer: number) {
     this.id = setTimeout(handler, timer)
     this.handler = handler;
+    this.start = Date.now();
+    this.timeLeft = timer;
   }
   clear() {
     clearTimeout(this.id)
+  }
+  pause() {
+    const passed = Date.now() - this.start;
+    this.timeLeft = this.timeLeft - passed;
+    this.clear();
+  }
+  continue() {
+    this.clear();
+    this.id = setTimeout(this.handler, this.timeLeft);
+    this.start = Date.now();
   }
 } 
